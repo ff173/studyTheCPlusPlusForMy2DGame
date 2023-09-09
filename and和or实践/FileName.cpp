d7667fc1f1ad8fd72698f080ebaec0a3ba3232ff
flying00cat <1733128581@qq.com>
2023-09-10 00:07:44 +0800

    学习C plus plus 时对and和or实践使用

---
+++ and和or实践/FileName.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i = 1;
+	int j = 2;
+	int k = 3;
+
+	if (i < k || i == j)
+	{
+		cout << "This will be prined." << endl;
+	}
+
+	system("pause");
+}
